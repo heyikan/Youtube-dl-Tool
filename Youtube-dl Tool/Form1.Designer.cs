bf682270ad7ef1114004b91c4cf2a8659131c6af
heyikan <hkayakoku@gmail.com>
2016-08-14 21:02:47 +0300

    processStart

--- Youtube-dl Tool/Form1.Designer.cs
+++ Youtube-dl Tool/Form1.Designer.cs
@@ -28,21 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(104, 13);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(343, 20);
+            this.textBoxURL.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(469, 13);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(81, 20);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "GO";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(632, 457);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxURL);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStart;
     }
 }
 
--- Youtube-dl Tool/Form1.cs
+++ Youtube-dl Tool/Form1.cs
@@ -10,8 +10,12 @@ using System.Windows.Forms;
 
 namespace Youtube_dl_Tool
 {
+    // TODO: Terminalden çalışma komutu eklenecek...
     public partial class Form1 : Form
     {
+        configuration config = new configuration();
+        controller control = new controller();
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +24,19 @@ namespace Youtube_dl_Tool
         private void Form1_Load(object sender, EventArgs e)
         {
             Console.WriteLine("deneme");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            config.URL = textBoxURL.Text;
+
+            control.startYoutubeDl(config.URL);
+            
         }
     }
 }
